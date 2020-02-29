88a9a2bc29309e3fa7e6b95fa435613a81b19a1f
mnwg <adammnwg@gmail.com>
2020-02-29 11:22:43 +0100

    first

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -6,6 +6,16 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        BarChart barChart = new BarChart("Task types report",new ArrayList());
+        BarChart barChart = new BarChart("Task types report", new ArrayList());
+        BarChart barChart1 = BarChart.builder()
+                .withTitle("Title1")
+                .withbackgroundColor("Blue")
+                .withFontColor("Black")
+                .withheight(8)
+                .withwidth(5)
+                .build();
+        System.out.println(barChart);
+        System.out.println(barChart1);
+
     }
 }
